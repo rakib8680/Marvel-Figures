3362c7b64ec890599a077b5f5299eb84d83ac05d
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-18 16:56:35 +0600

    project setup completed

---
+++ src/components/layout/Home/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            
+            <Home></Home>
+
+        </div>
+    );
+};
+
+export default HomeLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomeLayout from './components/layout/Home/HomeLayout.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout></HomeLayout>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
